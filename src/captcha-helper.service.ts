ed47c00ca2de377400d96e932586c98314e9c413
Etienne Le Sueur <elesueur@vmware.com>
2018-08-20 15:47:25 -0400

    Run remote script outside of the Angular zone
    Running this script inside angular zone causes problems with Protractor
    because there are a lot of setTimeout uses in the remote script.

--- src/captcha-helper.service.ts
+++ src/captcha-helper.service.ts
@@ -1,18 +1,23 @@
-import { Injectable } from '@angular/core';
+import { NgZone, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class CaptchaHelperService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private ngZone: NgZone) { }
 
   // get script and execute it immediately
   getScript(url: string, onLoadSuccess: () => void): void {
     this.http.get(url, { responseType: 'text' })
       .subscribe(
         scriptString => {
-          let f = new Function(scriptString); f();
-          setTimeout(onLoadSuccess, 200);
+          let f = new Function(scriptString);
+            this.ngZone.runOutsideAngular(() => {
+              f();
+              this.ngZone.run(() => {
+                  setTimeout(onLoadSuccess, 200);
+              });
+            });
         }
       );
   }
